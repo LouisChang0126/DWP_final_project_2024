d87fda829213f73360a8b202916031de99a56637
司徒立中 <rain.sytwu@gmail.com>
2024-12-11 02:06:32 +0800

    [Update] Basic html, css, and js
    Including:
    1. home.html
    2. event.html

---
+++ js/side_bar.js
@@ -0,0 +1,22 @@
+const menuItems = document.querySelectorAll('.menu_item');
+
+menuItems.forEach(item =>
+{
+    item.addEventListener('click', function (e)
+    {
+        e.preventDefault();
+        console.log(this.id);
+
+        document.querySelectorAll('.section').forEach(section =>
+        {
+            section.classList.add('hidden');
+        });
+
+        const targetId = this.id;
+        const targetSection = document.getElementById(targetId);
+        if (targetSection)
+            {
+            targetSection.classList.remove('hidden');
+        }
+    });
+});
