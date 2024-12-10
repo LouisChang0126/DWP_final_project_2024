d9d687a56bd8e90ad35cff7c619de4dd886ad5b0
司徒立中 <rain.sytwu@gmail.com>
2024-12-11 02:13:12 +0800

    [Update] Simplify Code

--- js/table.js
+++ js/table.js
@@ -1,13 +1,16 @@
-// 獲取所有擁有 "available" 或 "unavailable" 類別的 td 元素
 const cells = document.querySelectorAll('td.available, td.unavailable');
 
-cells.forEach(cell => {
-    cell.addEventListener('click', function() {
-        // 如果當前是 "available"，則改為 "unavailable"，反之亦然
-        if (this.classList.contains('available')) {
+cells.forEach(cell =>
+{
+    cell.addEventListener('click', function()
+    {
+        if (this.classList.contains('available'))
+        {
             this.classList.remove('available');
             this.classList.add('unavailable');
-        } else if (this.classList.contains('unavailable')) {
+        }
+        else if (this.classList.contains('unavailable'))
+        {
             this.classList.remove('unavailable');
             this.classList.add('available');
         }
